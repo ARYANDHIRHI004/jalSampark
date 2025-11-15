cc8fc3fd64760e416986c254547bb7b33cf79bf4
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-11-15 14:39:51 +0530

    backend jal

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(
 
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 import authRouter from "./routes/auth.routes.js";
